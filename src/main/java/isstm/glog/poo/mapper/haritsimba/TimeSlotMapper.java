e38fd1097e2842b5368a61ecab2e16e5c77e5b20
haritsimba <haritsimbafranck@gmail.com>
2025-04-20 22:00:43 +0300

    mapper pour le timeSlot

--- src/main/java/isstm/glog/poo/mapper/haritsimba/TimeSlotMapper.java
+++ src/main/java/isstm/glog/poo/mapper/haritsimba/TimeSlotMapper.java
@@ -1,4 +1,35 @@
 package isstm.glog.poo.mapper.haritsimba;
 
+import isstm.glog.poo.dtos.haritsimba.response.TimeSlotRepresentation;
+import isstm.glog.poo.entities.haritsimba.Classe;
+import isstm.glog.poo.entities.haritsimba.Subject;
+import isstm.glog.poo.entities.haritsimba.Teacher;
+import isstm.glog.poo.entities.haritsimba.TimeSlot;
+
 public class TimeSlotMapper {
+    public static TimeSlotRepresentation mapToRepresentation(TimeSlot timeSlot){
+        TimeSlotRepresentation representation = new TimeSlotRepresentation();
+        representation.setId(timeSlot.getId());
+        representation.setDay(timeSlot.getDay().toString());
+        representation.setStartTime(timeSlot.getStartTime());
+        representation.setEndTime(timeSlot.getEndTime());
+
+        Teacher teacher = (Teacher) timeSlot.getTeacher();
+        Classe classe = (Classe) timeSlot.getClasse();
+        Subject subject = (Subject) timeSlot.getSubject();
+        if(teacher!=null){
+            representation.setTeacherId(teacher.getId());
+            representation.setTeacherDisplayName(teacher.getName());
+        }
+        if(classe!=null){
+            representation.setClasseId(classe.getId());
+            representation.setClasseDepartment(classe.getParcours());
+            representation.setClasseLevel(classe.getNiveau());
+            representation.setClasseDisplayName(classe.getNiveau()+" "+classe.getParcours());
+        }
+        representation.setSubjectId(subject.getId());
+        representation.setSubjectDisplayName(subject.getName());
+
+        return  representation;
+    }
 }
