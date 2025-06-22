44a20bbf01e7a21a538b5275aa2bffde3e30fb79
zikrea <zikrea@customer.lan>
2025-06-22 16:01:52 -0400

    dina

--- src/main/java/isstm/glog/poo/PooApplication.java
+++ src/main/java/isstm/glog/poo/PooApplication.java
@@ -2,6 +2,9 @@ package isstm.glog.poo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class PooApplication {
@@ -10,4 +13,17 @@ public class PooApplication {
 		SpringApplication.run(PooApplication.class, args);
 	}
 
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+          .addMapping("/api/dina/**")
+          .allowedOrigins("http://localhost:3000")
+          .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+          .allowCredentials(true);
+      }
+    };
+  }
 }
